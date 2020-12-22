88d94cee5f15bba23c384ea92b89799798b9d8fd
David <khachatryandavid1@gmail.com>
2020-12-22 15:49:03 -0600

    Edit

--- users/views.py
+++ users/views.py
@@ -3,24 +3,34 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+from django.views.decorators.csrf import csrf_protect
 
-
+@csrf_protect
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
+        
         if form.is_valid():
-            form.save()
             username = form.cleaned_data.get('username')
             email = form.cleaned_data.get('email')
             emails = User.objects.filter(email=email).first()
             if isinstance(emails, type(None)):
                 messages.success(request, f'Account created for {username}!')
+                form.save()
                 return redirect('login')
             args = {}
             text = "Email is taken"
             args['alert'] = text
             return render(request, 'users/register.html', args)
         else:
+            password1 = form.cleaned_data.get('password1')
+            password2 = form.cleaned_data.get('password2')
+
+            if password1 != password2:
+                args = {}
+                text = "Password Do Not Match"
+                args['alert'] = text
+                return render(request, 'users/register.html', args)
             args = {}
             text = "Username is taken"
             args['alert'] = text
